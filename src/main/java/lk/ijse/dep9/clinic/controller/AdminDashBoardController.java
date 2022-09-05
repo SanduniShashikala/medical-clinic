beb28fca0426f5163127d4a6f07d3d326bbbe579
Sanduni Nagodage <sanduninagodage@gmail.com>
2022-09-05 17:01:42 +0530

    Close #7

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashBoardController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashBoardController.java
@@ -1,4 +1,19 @@
 package lk.ijse.dep9.clinic.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class AdminDashBoardController {
+    public Button btnProfileManage;
+    public Button btnViewRecods;
+    public Button btnSetting;
+
+    public void btnProfileManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewRecodsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingOnAction(ActionEvent actionEvent) {
+    }
 }
