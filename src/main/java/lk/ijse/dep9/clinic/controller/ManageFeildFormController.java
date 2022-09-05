bd62c321d1fa03b82a4451604ab723289304dfc6
Sanduni Nagodage <sanduninagodage@gmail.com>
2022-09-05 17:43:08 +0530

    Close #12

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/ManageFeildFormController.java
@@ -0,0 +1,23 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ManageFeildFormController {
+    public Button btnSave;
+    public Button btnNew;
+    public Button btnDelete;
+    public TextField txtInput;
+    public ListView lblOutput;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/lk/ijse/dep9/clinic/controller/SettingFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/SettingFormController.java
@@ -1,4 +1,29 @@
 package lk.ijse.dep9.clinic.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class SettingFormController {
+    public AnchorPane pngContainer;
+    public Button btnManagePrice;
+    public Button btnAddDiscount;
+    public Button btnManageFeild;
+    public Button btnChangePassword;
+    public Button btnAbout;
+
+    public void btnManagePriceOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddDiscountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageFeildOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnChangePasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+    }
 }
